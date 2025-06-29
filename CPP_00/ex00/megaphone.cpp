a54cb5240bf6fea91ccba6df311a2b03e61dcf03
nachat-ayoub <ayoub.nachat.27@gmail.com>
2025-06-29 13:45:14 +0100

    test cpp

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+	std::cout << "Number of arguments: " << argc << std::endl;
+
+	for (int i = 0; i < argc; ++i) {
+		std::cout << "Arg " << i << ": " << argv[i] << std::endl;
+	}
+
+	return 0;
+}
